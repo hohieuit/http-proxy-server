c61a35cd4aefca603ee38030ae0b543ae2380b6e
hieu.ho <hieu.ho@opskins.com>
2022-04-27 10:03:05 +0700

    add dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const httpProxy = require("http-proxy");
 
 const TARGET_URL = process.env.TARGET_URL || "https://stg-inventory-api.thh.io";
+const ORIGIN_URL=process.env.ORIGIN_URL || '*';
 const PORT = process.env.PORT || 8000;
 const proxy = httpProxy
   .createProxyServer({
@@ -10,7 +12,7 @@ const proxy = httpProxy
   .listen(PORT);
 
 proxy.on("proxyRes", function (proxyRes: any, req: any, res: any) {
-  proxyRes.headers["access-control-allow-origin"] = "*";
+  proxyRes.headers["access-control-allow-origin"] = ORIGIN_URL;
   proxyRes.headers["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS";
 });
 
